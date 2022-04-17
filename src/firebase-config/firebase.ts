91a7d3ccf3a21f1d2ac7b22da494ff264e828dd6
Довлет Джумаев <danizold@gmail.com>
2022-04-18 02:31:26 +0300

    added firebase-config

---
+++ src/firebase-config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+});
+
+const db = firebase.firestore();
+
+export const auth = app.auth();
+export const dbBoards = db;
+export default app;
